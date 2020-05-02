7cc14474146c7376ce99f93858ca4c775caeb8b0
Edyta Wojdala <edytawoj095@gmail.com>
2020-05-02 11:51:54 +0200

    Add game code

--- js/script.js
+++ js/script.js
@@ -23,3 +23,37 @@ if (randomNumber == 1) {
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+    printMessage('nieznany ruch');
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Wygrywa komputer!');
+}
+
+if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Wygrywa komputer');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}
+
+if (computerMove == playerMove) {
+    printMessage('Remis');
+}
